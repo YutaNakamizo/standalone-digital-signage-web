d6091887a129903bd0b8abc15af29419947be786
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2020-09-24 12:13:30 +0900

    #1 Routing と各ルートコンポーネント作成完了

---
+++ src/routes/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Div100vh from 'react-div-100vh';
+
+export const Display = () => {
+  return (
+    <Div100vh className="Display">
+      <div>Display</div>
+    </Div100vh>
+  );
+};
+
